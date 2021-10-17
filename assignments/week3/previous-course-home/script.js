61681049118f02f24d95c73643655d006e07a03a
Brandon LaDuke <brandon@brandonladuke.com>
2021-10-17 00:39:14 -0400

    Move course home to prepare assn0302

---
+++ assignments/week3/previous-course-home/script.js
@@ -0,0 +1,15 @@
+// assignment 1.3
+function mouseOver1() {
+  document.getElementById("a1-3").classList.add("show");
+}
+function mouseOut1() {
+  document.getElementById("a1-3").classList.remove("show");
+}
+
+//assignment 1.4
+function mouseOver2() {
+  document.getElementById("a1-4").classList.add("show");
+}
+function mouseOut2() {
+  document.getElementById("a1-4").classList.remove("show");
+}
