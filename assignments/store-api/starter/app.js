4a303ed7f9547707b2c72c9beed1e72f479d707e
Brandon LaDuke <brandon@brandonladuke.com>
2021-11-27 09:05:45 -0500

    set up database connection

--- assignments/store-api/starter/app.js
+++ assignments/store-api/starter/app.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -28,6 +30,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
   try {
     // Connection DB
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`Server is listening to port ${port}...`))
   } catch (error) {
     console.log(error)
@@ -37,3 +40,5 @@ const start = async () => {
 }
 
 start();
+
+// pass: y7quZB7eEaKuZEM
