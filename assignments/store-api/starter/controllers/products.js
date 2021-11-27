4a6d61ab50a0efc901e8b26e4ccbbefb77a55ffc
Brandon LaDuke <brandon@brandonladuke.com>
2021-11-27 10:29:06 -0500

    async errors handled

--- assignments/store-api/starter/controllers/products.js
+++ assignments/store-api/starter/controllers/products.js
@@ -1,9 +1,10 @@
 const getAllProductsStatic = async (req,res) =>  {
-  res.status(200).json({msg:'products testing route'})
+  throw new Error('testing async errors');
+  res.status(200).json({msg:'products testing route'});
 }
 
 const getAllProducts = async (req,res) =>  {
-  res.status(200).json({msg:'products route'})
+  res.status(200).json({msg:'products route'});
 }
 
 module.exports = {
