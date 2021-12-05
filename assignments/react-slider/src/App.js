276153cfbaf3d904e8d1585d11c4124e59c3d346
Brandon LaDuke <brandon@brandonladuke.com>
2021-12-05 06:48:04 -0500

    added ability for the slider to wrap and auto slide

--- assignments/react-slider/src/App.js
+++ assignments/react-slider/src/App.js
@@ -5,6 +5,24 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length -1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000); // 5 Seconds because 3 seconds does not give enough time to read
+    return ()=> clearInterval(slider);
+  }, [index]);
+
   return (
     <section className='section'>
       <div className='title'>
